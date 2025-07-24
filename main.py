d8fa44d15f5f4d40ddac143b472296916310a200
ishita051 <ishitasrivastava993@gmail.com>
2025-07-24 12:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import threading
 import tkinter as tk
-from ui import KarbonUI
-from preview import webview_main
+import threading
+from ui_items.karbon_ui import KarbonUI
+from web_server.server import webview_main
+import sys
+import os
+
+# Function to get the correct path for bundled data files
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 if __name__ == "__main__":
     def start_ui():
         root = tk.Tk()
         app = KarbonUI(root)
-        root.protocol("WM_DELETE_WINDOW", root.destroy)
+        root.protocol("WM_DELETE_WINDOW", app.on_closing)
         root.mainloop()
 
-    threading.Thread(target=start_ui, daemon=True).start()
+    # Run Tkinter in a separate thread
+    ui_thread = threading.Thread(target=start_ui)
+    ui_thread.daemon = True
+    ui_thread.start()
 
-    # start webview in main thread
+    # Run Flask and webview in the main thread
     webview_main()
